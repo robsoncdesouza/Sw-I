360614b097302974252846266574f9d45a648939
Robson Cardoso <githubrobsoncardoso@gmail.com>
2026-03-05 08:51:40 -0300

    Add files via upload

---
+++ aula04/exemplo1.php
@@ -0,0 +1,12 @@
+<?php
+    $a = 10;
+    $b = "10";
+
+    $result = ($a == $b);
+    if($result == 1) {
+        echo "Resultado = Verdadeiro";
+    }else {
+        echo "Resultado = Falso";
+    }
+
+?>
---
+++ aula04/exercicio1.php
@@ -0,0 +1,14 @@
+<?php
+    $nome = "Tyrone";
+    $idade = 18;
+    $salario = 100;
+
+    echo "Oi! Qual o seu nome?";
+    echo "<br>$nome</br>";
+    echo "Oi $nome! Quantos anos você tem?"; 
+    echo "<br>$idade</br>";
+    echo "Então você tem $idade +!!!";
+    echo "Quanto você ganha $nome?";
+    echo "<br>$salario</br>";
+    echo "$salario! + Eu espero que seja por hora e não por ano!!!! rsrsrs....";
+?>
